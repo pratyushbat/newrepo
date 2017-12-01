0bc7e5ef115df3875bc2975350368d7457f4d202
pratyush choudhary <pratyush3030@gmail.com>
2017-12-01 20:28:37 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './documents/home/home.component';
+import {LocationStrategy, HashLocationStrategy} from '@angular/common';
+import {Routes,RouterModule} from '@angular/router';
+import { ServicesComponent } from './documents/services/services.component';
 
+const routes:Routes=[
+  {path: '', component: HomeComponent},
+  {path: 'services', component: ServicesComponent }
+];
+
+const routing = RouterModule.forRoot(routes);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServicesComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  imports: [  BrowserModule,routing  ],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy} ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
